c3343e8a45f8baf77758f71f6a3ca6c008f79e98
bielmartinez <bielmartinezcaceres16@gmail.com>
2024-12-04 19:38:18 +0100

    implementacio funcions per forms

--- Wonderful travel/Controlador/controlador.php
+++ Wonderful travel/Controlador/controlador.php
@@ -15,4 +15,45 @@ function arreglarDades($input)
     return $input;
 }
 
+
+function selectContinent()
+{
+    $connexio = conectarBD();
+    $consulta = $connexio->prepare('SELECT DISTINCT continent FROM paisos');
+    $consulta->execute();
+    $continent = $consulta->fetchAll(PDO::FETCH_ASSOC);
+    return $continent;
+}
+
+function selectPais($continent)
+{
+    $connexio = conectarBD();
+    $consulta = $connexio->prepare('SELECT nom FROM paisos WHERE continent = :continent');
+    $consulta->bindParam(':continent', $continent, PDO::PARAM_STR);
+    $consulta->execute();
+    $pais = $consulta->fetchAll(PDO::FETCH_ASSOC);
+    return $pais;
+}
+
+//Crea una funció per a seleccionar el preu del viatge segons el país que haguem seleccionat
+function selectPreu($pais)
+{
+    $connexio = conectarBD();
+    $consulta = $connexio->prepare('SELECT preu FROM paisos WHERE nom = :pais');
+    $consulta->bindParam(':pais', $pais, PDO::PARAM_STR);
+    $consulta->execute();
+    $preu = $consulta->fetch(PDO::FETCH_ASSOC);
+    return $preu;
+}
+
+//Crea una funció per a seleccionar la imatge del país que haguem seleccionat
+function selectImatge($pais)
+{
+    $connexio = conectarBD();
+    $consulta = $connexio->prepare('SELECT imatge FROM paisos WHERE nom = :pais');
+    $consulta->bindParam(':pais', $pais, PDO::PARAM_STR);
+    $consulta->execute();
+    $imatge = $consulta->fetch(PDO::FETCH_ASSOC);
+    return $imatge;
+}
 ?>
